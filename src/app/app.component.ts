3be59c960ff594eb5ca6dbb76110d1e8174871f1
oiDreas <andreasanthony92@gmail.com>
2023-05-11 20:32:23 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'frontend';
 }
+
+var variavel = 0;
+var variavel = 10;
+console.log('variável', variavel);
+
+function funcao() {
+  var outra_variavel = 20;
+  var outra_variavel = 30;
+  console.log('outra variável: ',outra_variavel);
+}
+funcao();
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  nomeEmpresa: string = 'Marombas da Programação';
+  nomeFunc: string = 'Agostinho';
+  sobrenomeFunc: string = 'Carrara';
+  idFunc: number = 1;
+  salFunc: number = 3333.32;
+  status: boolean = false;
+  nomeCompleto: string = ` Nome do Funcionário:${this.nomeFunc} ${this.sobrenomeFunc}`;
+
+
+    constructor() {
+      /*this.nomeFunc = 'Agostinho'
+      *this.nomeCompleto = this.nomeFunc + ' ' + this.sobrenomeFunc;
+      */
+     console.log (this.nomeCompleto);
+     console.log ('Nome da empresa', this.nomeEmpresa);
+     console.log (`Salario: ${this.salFunc}`);
+     console.log (`Id:${this.idFunc}`);
+     console.log (`Status: ${this.status}`);
+
+      var variavel1;
+      let variavel2;
+      const variavel3 = 3;
+    }
+
+
 
 }
