be55c1bfeb1ddb0563da9674f80f04fd0d168448
oiDreas <andreasanthony92@gmail.com>
2023-05-18 22:11:54 -0300

    Atualização

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,36 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent  {
 
-  nomeEmpresa: string = 'Marombas da Programação';
-  nomeFunc: string = 'Agostinho';
-  sobrenomeFunc: string = 'Carrara';
+  nomeEmpresa: string = 'HAS Company';
+  nomeFunc: string = 'Joao';
+  sobrenomeFunc: string = "Silva";
   idFunc: number = 1;
-  salFunc: number = 3333.32;
-  status: boolean = false;
-  nomeCompleto: string = ` Nome do Funcionário:${this.nomeFunc} ${this.sobrenomeFunc}`;
-
+  salFunc: number= 2000.00;
+  status: boolean = true;
+  nomeCompleto: string = `Nome do funcionario: ${this.nomeFunc} ${this.sobrenomeFunc}`;
+  nomeProduto: string = "Curso de Angular";
+  anuncio: string = `O ${this.nomeProduto} está em promoção`;
+  idProduto: number = 3;
+  precoProduto: number = 2.59;
+  promocao: boolean = false;
+  foto:string = 'assets/img/crud.png'
+  dataValidade = '2021-12-31'
 
     constructor() {
-      /*this.nomeFunc = 'Agostinho'
-      *this.nomeCompleto = this.nomeFunc + ' ' + this.sobrenomeFunc;
-      */
-     console.log (this.nomeCompleto);
-     console.log ('Nome da empresa', this.nomeEmpresa);
-     console.log (`Salario: ${this.salFunc}`);
-     console.log (`Id:${this.idFunc}`);
-     console.log (`Status: ${this.status}`);
 
-      var variavel1;
-      let variavel2;
-      const variavel3 = 3;
     }
-
-
 
 }
